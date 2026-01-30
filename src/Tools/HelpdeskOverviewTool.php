7a9b4c92fba99a5ba696ad431203b540b0acc511
Martin Erren <martin3r@Mac.fritz.box>
2026-01-30 18:41:46 +0100

    tickets sperren

--- src/Tools/HelpdeskOverviewTool.php
+++ src/Tools/HelpdeskOverviewTool.php
@@ -23,7 +23,7 @@ class HelpdeskOverviewTool implements ToolContract, ToolMetadataContract
     {
         return [
             'type' => 'object',
-            'properties' => [],
+            'properties' => new \stdClass(),
             'required' => [],
         ];
     }
